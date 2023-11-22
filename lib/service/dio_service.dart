daaba9d15a832d45cb0d40f66cec71263f9bbba6
UkiDelly <daehyeon1999@naver.com>
2023-11-22 20:57:23 +0900

    ✨ dio service 추가

---
+++ lib/service/dio_service.dart
@@ -0,0 +1,22 @@
+import 'package:awesome_dio_interceptor/awesome_dio_interceptor.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dio_service.g.dart';
+
+@riverpod
+Dio dio(DioRef ref) {
+  final dio = Dio(BaseOptions(
+    baseUrl: "http://localhost:8000",
+    contentType: "application/json",
+  ));
+
+  dio.interceptors.add(AwesomeDioInterceptor(
+      logRequestTimeout: false,
+      logResponseHeaders: false,
+      logRequestHeaders: true,
+      logger: debugPrint));
+
+  return dio;
+}
