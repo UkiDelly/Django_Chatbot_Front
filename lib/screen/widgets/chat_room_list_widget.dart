29267c64daa0df04d21f5d6aa43c369866c42d41
UkiDelly <daehyeon1999@naver.com>
2023-11-22 22:05:51 +0900

    💄 ChatRoomList 위젯 개발중
    - Header 부분 개발중

--- lib/screen/widgets/chat_room_list_widget.dart
+++ lib/screen/widgets/chat_room_list_widget.dart
@@ -1,3 +1,4 @@
+import 'package:django_chatbot_front/utils/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,7 +11,25 @@ class ChatRoomListWidget extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          Flexible(flex: 1, child: Placeholder()),
+          Flexible(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+                child: Row(
+                  children: [
+                    Text(
+                      "채팅",
+                      style: context.textTheme.displaySmall!.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.create),
+                      iconSize: 25,
+                    )
+                  ],
+                ),
+              )),
           SizedBox(height: 20.h),
           Flexible(flex: 8, child: Placeholder()),
           SizedBox(height: 20.h),
