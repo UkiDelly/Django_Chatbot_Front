30f462b81ec20b2e8197990708636fa61fff9e90
UkiDelly <daehyeon1999@naver.com>
2023-11-23 16:06:03 +0900

    ✨ Supabase 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:django_chatbot_front/common/routing.dart';
 import 'package:django_chatbot_front/common/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env["SUPABASE_URL"]!,
+    anonKey: dotenv.env["SUPABASE_ANON_KEY"]!,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
