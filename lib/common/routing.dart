0e09adac74aa37ed7b944911979b293354e43a32
UkiDelly <daehyeon1999@naver.com>
2023-11-24 09:07:23 +0900

    🎨 로그인 되어 있지 않으면 로그인 스크린으로 리다이렉트를 루트단으로 변경

--- lib/common/routing.dart
+++ lib/common/routing.dart
@@ -1,4 +1,3 @@
-import 'package:django_chatbot_front/models/user_model.dart';
 import 'package:django_chatbot_front/screen/login/login_screen.dart';
 import 'package:django_chatbot_front/screen/main/main_screen.dart';
 import 'package:django_chatbot_front/screen/register/register_sreen.dart';
@@ -6,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../models/user_model.dart';
 import '../service/user_service.dart';
 
 part 'routing.g.dart';
@@ -18,21 +18,22 @@ GoRouter goRouter(GoRouterRef ref) {
     navigatorKey: rootNavKey,
     initialLocation: LoginScreen.routePath,
     debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final userState = ref.watch(userStateServiceProvider).value!;
+
+      if (userState is UserModelEmpty || userState is UserModelError) {
+        return LoginScreen.routePath;
+      } else {
+        return null;
+      }
+    },
     routes: [
       // 메인 페이지
       GoRoute(
-          path: MainScreen.routePath,
-          name: MainScreen.routeName,
-          pageBuilder: (context, state) => const NoTransitionPage(child: MainScreen()),
-          redirect: (context, state) {
-            final userState = ref.watch(userStateServiceProvider).value!;
-
-            if (userState is UserModelEmpty || userState is UserModelError) {
-              return LoginScreen.routePath;
-            } else {
-              return null;
-            }
-          }),
+        path: MainScreen.routePath,
+        name: MainScreen.routeName,
+        pageBuilder: (context, state) => const NoTransitionPage(child: MainScreen()),
+      ),
 
       // 로그인 페이지
       GoRoute(
