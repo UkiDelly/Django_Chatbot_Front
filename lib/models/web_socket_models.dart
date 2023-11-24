7c5d505f2074b54ccdd116a31293ebe95bd15036
UkiDelly <daehyeon1999@naver.com>
2023-11-24 16:07:46 +0900

    🏷️💄 웹소켓 전송/수신 모델 구현

---
+++ lib/models/web_socket_models.dart
@@ -0,0 +1,24 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'web_socket_models.freezed.dart';
+part 'web_socket_models.g.dart';
+
+@freezed
+class WebSocketSend with _$WebSocketSend {
+  const WebSocketSend._();
+
+  const factory WebSocketSend({
+    required String message,
+  }) = _WebSocketSend;
+
+  factory WebSocketSend.fromJson(Map<String, dynamic> json) => _$WebSocketSendFromJson(json);
+}
+
+@freezed
+class WebSocketRecieve with _$WebSocketRecieve {
+  const WebSocketRecieve._();
+
+  const factory WebSocketRecieve({required String content}) = _WebSocketRecieve;
+
+  factory WebSocketRecieve.fromJson(Map<String, dynamic> json) => _$WebSocketRecieveFromJson(json);
+}
